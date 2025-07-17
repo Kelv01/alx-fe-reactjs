17ef8a9bbc48f3fd70618e0e6d9e42ca34dbd0cd
kelv01 <kelvinnjugunanungari@gmail.com>
2025-07-17 09:42:16 +0300

    added component styles

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return(
+        <footer style={{fontSize: '12px', padding: '12px', textAlign: 'center'}}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
+
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+    return(
+        <main style={{padding: '12px', margin: '8px', fontSize: '18px'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+export default MainContent;
