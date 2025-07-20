7c0884a05e506a952adb8ae1807b16233602c6bc
kelv01 <kelvinnjugunanungari@gmail.com>
2025-07-20 20:17:49 +0300

    replaced pop drilling with context method

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,7 +5,7 @@ import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
 import ProfilePage from "./components/ProfilePage";
-import UserContext from "./contexts/UserContext";
+import UserContext from "./UserContext";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
