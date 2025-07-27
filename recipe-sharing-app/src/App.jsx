d7f8270326d3d261d88dae1006b9651bda9f4849
kelv01 <kelvinnjugunanungari@gmail.com>
2025-07-27 23:18:03 +0300

    added components

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <RecipeList />
+        <AddRecipeForm />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
