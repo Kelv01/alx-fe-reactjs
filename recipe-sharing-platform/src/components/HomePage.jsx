25d64a516e41db066b2422c278daf00620eea56c
kelv01 <kelvinnjugunanungari@gmail.com>
2025-08-12 10:45:17 +0300

    added hover state

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -11,12 +11,12 @@ function HomePage() {
 
     },[])
 
-  return <div className=" flex  gap-6 bg-slate-800 items-center justify-center h-screen">
+  return <div className=" flex gap-6  bg-slate-800 items-center justify-center h-screen">
     {recipeData.map((recipe, idx) => (
-         <div key={idx} className="hover:bg-indigo-400  items-center bg-slate-300 rounded-xl shadow-md h-96 text-center">
+         <div key={idx} className="h-60 w-full grid grid-cols-2 gri md:grid-cols-4 hover:bg-indigo-400  items-center bg-slate-300 rounded-xl shadow-md  text-center">
+            <img className="h-24 w-24" src={recipe.image} alt={recipe.title} />
             <h1 className="items-center text-purple-600 justify-center">{recipe.title}</h1>
-            <p>{recipe.summary}</p>
-            <img src={recipe.image} alt={recipe.title} />
+            <p className="">{recipe.summary}</p>
          </div>
     ))}
 
