62a336b10e0524817b5ad5879f15709089f02487
kelv01 <kelvinnjugunanungari@gmail.com>
2025-08-12 10:08:00 +0300

    maped json data to homepage component

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -11,9 +11,9 @@ function HomePage() {
 
     },[])
 
-  return <div className="flex  gap-6 bg-slate-800 items-center justify-center h-screen">
+  return <div className=" flex  gap-6 bg-slate-800 items-center justify-center h-screen">
     {recipeData.map((recipe, idx) => (
-         <div key={idx} className="bg-slate-300 rounded-xl shadow-md h-96">
+         <div key={idx} className="hover:bg-indigo-400  items-center bg-slate-300 rounded-xl shadow-md h-96 text-center">
             <h1 className="items-center text-purple-600 justify-center">{recipe.title}</h1>
             <p>{recipe.summary}</p>
             <img src={recipe.image} alt={recipe.title} />
