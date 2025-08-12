2d8b3de6ff68f7c76ddfea8ec77b22664cf7bd61
kelv01 <kelvinnjugunanungari@gmail.com>
2025-08-12 09:59:31 +0300

    maped json data to homepage component

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import data from '../data.json'
+
+import { useState, useEffect } from "react";
 
 function HomePage() {
-  return <div className="text-blue-500">HomePage</div>;
+    const [recipeData, setRecipeData] = useState([])
+    
+    useEffect(() =>  {
+       setRecipeData(data)
+
+    },[])
+
+  return <div className="flex  gap-6 bg-slate-800 items-center justify-center h-screen">
+    {recipeData.map((recipe, idx) => (
+         <div key={idx} className="bg-slate-300 rounded-xl shadow-md h-96">
+            <h1 className="items-center text-purple-600 justify-center">{recipe.title}</h1>
+            <p>{recipe.summary}</p>
+            <img src={recipe.image} alt={recipe.title} />
+         </div>
+    ))}
+
+  </div>;
 }
 
 export default HomePage;
