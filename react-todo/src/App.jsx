ab5baef42bd3d07764570de33b56dc57c4bc0388
kelv01 <kelvinnjugunanungari@gmail.com>
2025-08-24 23:03:06 +0300

    firt commit

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import TodoList from "./components/TodoList"; // ✅ import TodoList
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Todo App</h1>
-      <TodoList /> {/* ✅ render TodoList */}
+    <div>
+      <h1>My App</h1>
+      <TodoList />
     </div>
   );
 }
