2c80bcd9d33e9c2c8f6db7e3ab53c83e380b34b2
kelv01 <kelvinnjugunanungari@gmail.com>
2025-08-24 22:51:59 +0300

    firt commit

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -10,29 +10,30 @@ describe("TodoList Component", () => {
     expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
   });
 
-  test("adds a new todo", () => {
+  test("can add a new todo", () => {
     render(<TodoList />);
-    const input = screen.getByPlaceholderText("Add a new todo");
-    const button = screen.getByText("Add");
+    const input = screen.getByPlaceholderText(/add new todo/i);
+    const addButton = screen.getByText(/add/i);
 
-    fireEvent.change(input, { target: { value: "New Todo" } });
-    fireEvent.click(button);
+    fireEvent.change(input, { target: { value: "Write tests" } });
+    fireEvent.click(addButton);
 
-    expect(screen.getByText("New Todo")).toBeInTheDocument();
+    expect(screen.getByText("Write tests")).toBeInTheDocument();
   });
 
-  test("toggles todo completion", () => {
+  test("can toggle a todo", () => {
     render(<TodoList />);
     const todo = screen.getByText("Learn React");
     fireEvent.click(todo);
-    expect(todo).toHaveStyle("text-decoration: line-through");
+    expect(todo).toHaveClass("completed"); // make sure your component applies this class
   });
 
-  test("deletes a todo", () => {
+  test("can delete a todo", () => {
     render(<TodoList />);
-    const todo = screen.getByText("Learn React");
-    const deleteButton = todo.querySelector("button");
+    const todo = screen.getByText("Build a Todo App");
+    const deleteButton = screen.getByLabelText(/delete Build a Todo App/i);
     fireEvent.click(deleteButton);
+
     expect(todo).not.toBeInTheDocument();
   });
 });
